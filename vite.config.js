f8256fa24edd4a11405bfad0c50bfa2a2513d10c
Jasveer Singh <entrepreneur.jasveer@gmail.com>
2025-03-05 13:12:36 +0530

    NG FIN MANAGER 2 update

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
 
+// Load environment variables
 dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/auth": process.env.REACT_APP_API_URL,
+  root: "./",
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: "index.html", // ✅ Ensure this points to the correct file
     },
   },
 });
